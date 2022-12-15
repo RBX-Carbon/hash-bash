c63d6380cb77172c71da9e13554b8c4b53010d99
RocketEcosystem <contact@rocketbunny.io>
2022-12-15 12:18:23 -0500

    initime removal

--- contracts/HashBash.sol
+++ contracts/HashBash.sol
@@ -6,21 +6,21 @@ contract HashBash {
     address public owner;                   // bigPoppa
 
     mapping (uint8 => bytes32) hashMash;    // mapping of all hashed answers
-    uint public initime;                    // keeps the normies in check at least
+    // uint public initime;                    // keeps the normies in check at least
 
     modifier bigPoppa {
         require(msg.sender == owner, '!owner');
         _;
     }
 
-    constructor(bytes32[12] memory hash_mash, uint _initime) {
+    constructor(bytes32[12] memory hash_mash, /* uint _initime */) {
         owner = msg.sender;
-        initime = _initime;
+        // initime = _initime;
         for (uint8 i = 0; i < hash_mash.length; i++) hashMash[i] = hash_mash[i];
     }
 
     function feelingLucky(uint8 pick, string calldata luckyGuess) public view returns (bool outcome) {
-        require(block.timestamp >= initime, '!initime');
+        // require(block.timestamp >= initime, '!initime');
         return keccak256(abi.encode(luckyGuess)) == hashMash[pick];
     }
 
